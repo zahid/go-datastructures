8b2998bf5fe704d2bdddac432243243438df2097
zahid mahir <zahid@zmhr.me>
2016-11-25 00:32:43 -0500

    Completed tests

--- stack/stack.go
+++ stack/stack.go
@@ -1,6 +1,4 @@
-package	stack
-
-import ("fmt")
+package stack
 
 /*
 We are implementing a First-In-First-Out stack
@@ -24,20 +22,17 @@ type Stack struct {
 }
 
 func (s *Stack) push(e Element) {
-	fmt.Printf("Pushing (%v, %T)\n", e, e)
 	s.elements = append(s.elements, e)
 }
 
 func (s *Stack) pop() Element {
 	top := s.elements[0]
 	s.elements = s.elements[1:]
-	fmt.Printf("Popped off %v\n", top)
 	return top
 }
 
 func (s *Stack) peek() Element {
 	top := s.elements[0]
-	fmt.Printf("Peeking at %v\n", top)
 	return top
 }
 
@@ -47,23 +42,4 @@ func (s *Stack) isEmpty() bool {
 
 func (s *Stack) clear() {
 	s.elements = Elements{}
-}
-
-func main() {
-	stackOfPapers := Stack{}
-
-	fmt.Printf("Stack isEmpty=%v\n", stackOfPapers.isEmpty())
-
-	stackOfPapers.push("bill")
-	stackOfPapers.push("reciept")
-	stackOfPapers.push("menu")
-
-	stackOfPapers.clear()
-
-	stackOfPapers.push("picture")
-
-	stackOfPapers.pop()
-
-	fmt.Printf("Stack isEmpty=%v\n", stackOfPapers.isEmpty())
-	fmt.Printf("stack=(%v, %T)\n", stackOfPapers, stackOfPapers)
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -6,14 +6,85 @@ import (
 
 func TestPush(t *testing.T) {
 	stackOfPapers := Stack{}
-
-	stackOfPapers.push("Phone number")
+	item := "Phone number"
+	stackOfPapers.push(item)
 
 	expectedSize := 1
 	actualSize := len(stackOfPapers.elements)
 
 	if expectedSize != actualSize {
-		t.Logf("Expected size of %v, but got %v\n", expectedSize, actualSize)
-		t.Fail()
+		t.Errorf("Expected size of %v, but got %v", expectedSize, actualSize)
+	}
+
+	if stackOfPapers.elements[0] != item {
+		t.Errorf("Expected the element to be %v, but got %v", item, stackOfPapers.elements[0])
+	}
+}
+
+func TestPop(t *testing.T) {
+	stackOfPapers := Stack{}
+	item := "Dinner check"
+	stackOfPapers.push(item)
+
+	poppedItem := stackOfPapers.pop()
+
+	// Check that poppedItem == item
+	if item != poppedItem {
+		t.Errorf("Item %v and popped item %v do not match", item, poppedItem)
+	}
+	// Check that the stack is empty
+	if len(stackOfPapers.elements) != 0 {
+		t.Errorf("Stack should be empty")
+	}
+}
+
+func TestPeek(t *testing.T) {
+	stackOfPapers := Stack{}
+	item := "Dry cleaning bill"
+	stackOfPapers.push(item)
+
+	peekedItem := stackOfPapers.peek()
+
+	// Check that peekedItem == item
+	if item != peekedItem {
+		t.Errorf("Item %v and peeked item %v do not match", item, peekedItem)
+	}
+
+	// Check that the stack is not empty
+	if len(stackOfPapers.elements) != 1 {
+		t.Errorf("Stack should not be empty")
+	}
+
+	// Check that the top element is still item
+	if item != stackOfPapers.elements[0] {
+		t.Errorf("The item should not have been removed from the stack")
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	stackOfPapers := Stack{}
+
+	// Stack should start as empty
+	if stackOfPapers.isEmpty() != true {
+		t.Errorf("Stack should be empty, but got %v instead", stackOfPapers)
+	}
+
+	stackOfPapers.push("Business card")
+
+	// Stack not be empty if an item is pushed to it
+	if stackOfPapers.isEmpty() == true {
+		t.Errorf("Stack should not be empty, but got %v instead", stackOfPapers)
+	}
+}
+
+func TestClear(t *testing.T) {
+	stackOfPapers := Stack{}
+
+	stackOfPapers.push("Old movie tickets")
+	stackOfPapers.clear()
+
+	// Should have no elements after clearing
+	if len(stackOfPapers.elements) != 0 {
+		t.Errorf("Stack should have no elements, instead it has %v", stackOfPapers)
 	}
 }
