31b4786081c924ac6156548705acf5a23cfadece
zahid mahir <zahid@zmhr.me>
2016-11-28 09:35:23 -0500

    Starting linked list implementation

---
+++ linkedlist/linkedlist.go
@@ -0,0 +1,16 @@
+package linkedlist
+
+type Node struct {
+	next  *Node
+	value interface{}
+}
+
+// push new element
+// find element e
+// get index
+
+type LinkedList struct {
+	next Node
+}
+
+func
